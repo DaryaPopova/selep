6dc3c5de8489cb146fc61d4d2a72c72fa9a27deb
DaryaPopova <pd2405@yandex.ru>
2023-03-13 17:51:38 +0100

    first code

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from selenium import webdriver
 
 
 def print_hi(name):
@@ -12,5 +13,9 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+
+    url = 'https://stepik.org/'
+    browser = webdriver.Chrome('/home/degu/chromedriver_linux64/chromedriver')
+    browser.get(url)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
