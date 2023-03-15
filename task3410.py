18d85fc507a93cb1fe81a269e4de34ed9788733e
DaryaPopova <pd2405@yandex.ru>
2023-03-15 17:36:16 +0100

    task 3.4.10

---
+++ task3410.py
@@ -0,0 +1,18 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+if __name__ == '__main__':
+    with webdriver.Chrome() as browser:
+        browser.get('https://parsinger.ru/selenium/6/6.html')
+        dropdown_value = ((12434107696 * 3) * 2) + 1
+        css_dropdown_value = "//option[text()='{}']".format(dropdown_value)
+        browser.find_element(By.ID, 'selectId').click()
+        browser.find_element(By.XPATH, css_dropdown_value).click()
+        browser.find_element(By.ID, 'sendbutton').click()
+        result_text = browser.find_element(By.ID, 'result').text
+        print(result_text)
+
+
+
