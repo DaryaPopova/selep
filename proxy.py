7cc70879aa45c86ced281f29d4c6a900ca17aca4
DaryaPopova <pd2405@yandex.ru>
2023-03-16 15:48:51 +0100

    just proxy

--- proxy.py
+++ proxy.py
@@ -3,7 +3,12 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 if __name__ == '__main__':
+    proxy = ['8.210.83.33:80']
     url = 'https://2ip.ru/'
+
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--proxy-server=%s' % proxy)
+
     with webdriver.Chrome() as browser:
         browser.get(url)
         time.sleep(5)
