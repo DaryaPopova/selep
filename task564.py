cb7628dfe55d869c8f9139adf20de3130edbe9d5
DaryaPopova <pd2405@yandex.ru>
2023-03-21 19:32:15 +0100

    cookie

--- task564.py
+++ task564.py
@@ -6,12 +6,12 @@ if __name__ == '__main__':
         browser.get('https://parsinger.ru/methods/5/index.html')
         links = browser.find_elements(By.CSS_SELECTOR, "div.urls a")
         max_date = 0
-        array = []
+        number = 0
         for link in links:
             link.click()
             expiration_date = browser.get_cookies()[0]['expiry']
-            array.append(expiration_date)
             if expiration_date > max_date:
                 max_date = expiration_date
+                number = browser.find_element(By.ID, 'result').text
             browser.back()
-        print(max_date)
+        print(number)
