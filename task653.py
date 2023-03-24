016205250c58e26f3d2c1a55e492c36fd1700740
DaryaPopova <pd2405@yandex.ru>
2023-03-24 15:14:11 +0100

    scroll

--- task653.py
+++ task653.py
@@ -4,22 +4,27 @@ from selenium.webdriver import Keys
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-if __name__ == '__main__':
+
+def count_spans_sum() -> int:
+    summ: int = 0
+    list_span = set()
     with webdriver.Chrome() as browser:
         browser.get('https://parsinger.ru/infiniti_scroll_1/')
-        summ = 0
         time.sleep(1)
-        list_span = []
-        while len(list_span) < 99:
-            spans = [x for x in browser.find_elements(By.CSS_SELECTOR, 'div#scroll-container span')]
+        while True:
+            spans = browser.find_elements(By.CSS_SELECTOR, 'div#scroll-container span')
             for span in spans:
                 if span.get_attribute('class') != 'last-of-list':
                     if span not in list_span:
                         summ += int(span.text)
                         span.find_element(By.TAG_NAME, 'input').send_keys(Keys.DOWN)
                         time.sleep(0.1)
-                        list_span.append(span)
+                        list_span.add(span)
                 else:
-                    break
-        summ += int(browser.find_element(By.CSS_SELECTOR, 'span.last-of-list').text)
-        print(summ)
+                    summ += int(browser.find_element(By.CSS_SELECTOR, 'span.last-of-list').text)
+                    return summ
+
+
+if __name__ == '__main__':
+    summ = count_spans_sum()
+    print(summ)
