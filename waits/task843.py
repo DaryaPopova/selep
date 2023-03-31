2ee2cd003a1746ccf67fb5c9ec673ecb1520e522
DaryaPopova <pd2405@yandex.ru>
2023-03-31 17:39:27 +0200

    waits

--- waits/task843.py
+++ waits/task843.py
@@ -1,6 +1,5 @@
 import time
 from selenium import webdriver
-from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
