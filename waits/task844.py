2654635d7044031f1ccc9d48827d6762558c2bcf
DaryaPopova <pd2405@yandex.ru>
2023-04-01 01:49:27 +0200

    waits

---
+++ waits/task844.py
@@ -0,0 +1,16 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
+if __name__ == '__main__':
+    with webdriver.Chrome() as browser:
+        browser.get('https://parsinger.ru/expectations/6/index.html')
+        time.sleep(1)
+        button = WebDriverWait(browser, 4, 1).until(EC.element_to_be_clickable(browser.find_element(By.ID, 'btn')))
+        button.click()
+        time.sleep(1)
+        element = WebDriverWait(browser, 25, 0.1)\
+            .until(EC.presence_of_element_located((By.CLASS_NAME, 'Y1DM2GR')))
+        print(element.text)
